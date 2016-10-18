8a8208e5e5c897132b15344c2c283d6b79864fb2
Deepak Bhaskaran <deepak.bhaskaran@gmail.com>
2016-10-18 01:32:44 -0400

    First template, first working url ready

--- phoneinfo/info/views.py
+++ phoneinfo/info/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.template import loader
 
 # Create your views here.
 
 from django.http import HttpResponse
+from models import PhoneInfo
 
 def index(request):
     return HttpResponse("Hello, you've reached the info channel!")
@@ -10,7 +12,13 @@ def index(request):
 def phone_number(request, phone_number):
     if phone_number:
         response = "This is the number you are requesting info on %s" % (phone_number)
+        rs = PhoneInfo.objects.filter(phone_number = phone_number)
+        context = {
+            'value': phone_number,
+            'count': rs.count(),
+        }
+        return render(request, 'info/index.html', context)
     else:
         response = "No number received and one is expected for sure."
+        return HttpResponse(response)
 
-    return HttpResponse(response)
