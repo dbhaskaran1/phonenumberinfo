bb590d13a58578edacd1acd3544c228eb6135ece
Deepak Bhaskaran <deepak.bhaskaran@gmail.com>
2016-10-18 03:26:33 -0400

    Working version, accepts number and shows info about the number

--- phoneinfo/info/urls.py
+++ phoneinfo/info/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^phone_number/(?P<phone_number>[0-9]+)$', views.phone_number, name='phone_number'),
+    url(r'^phone_number/(?P<phone_number>[\w]+)$', views.phone_number, name='phone_number'),
 ]
--- phoneinfo/info/views.py
+++ phoneinfo/info/views.py
@@ -5,6 +5,7 @@ from django.template import loader
 
 from django.http import HttpResponse
 from models import PhoneInfo
+from lib import phone_info
 
 def index(request):
     return HttpResponse("Hello, you've reached the info channel!")
@@ -13,11 +14,19 @@ def phone_number(request, phone_number):
     if phone_number:
         response = "This is the number you are requesting info on %s" % (phone_number)
         rs = PhoneInfo.objects.filter(phone_number = phone_number)
-        context = {
-            'value': phone_number,
-            'count': rs.count(),
-        }
-        return render(request, 'info/index.html', context)
+        number = phone_info.PhoneNumber(phone_number, 'US')
+        try:
+            result = number.get_data()
+            context = {
+                'value': phone_number,
+                'is_number_valid': result['is_number_valid']
+            }
+            return render(request, 'info/index.html', context)
+        except Exception as e:
+            response = "Bad number received." + str(e)
+            return HttpResponse(response)
+
+
     else:
         response = "No number received and one is expected for sure."
         return HttpResponse(response)
