98025909c170f2e38e2e3a623e2e78e43198e8c8
Deepak Bhaskaran <deepak.bhaskaran@gmail.com>
2016-10-18 16:28:15 -0400

    Added unit tests that hit each of the available end points

--- phoneinfo/info/tests.py
+++ phoneinfo/info/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+from models import PhoneInfo
+
+class NumberTestCase(TestCase):
+    def setUp(self):
+        PhoneInfo.objects.create(phone_number='6786786789', country='US', carrier='AT&T')
+
+    def test_phonenumberprops(self):
+        num = PhoneInfo.objects.get(phone_number='6786786789')
+        assert num.phone_number == '6786786789'
+        assert num.country == 'US'
+        assert num.carrier == 'AT&T'
+
+    def test_index_endpoint(self):
+        c = Client()
+        response = c.get('/info/')
+        assert response.status_code == 200
+
+    def test_date_endpoint(self):
+        c = Client()
+        response = c.get('/info/current/date')
+        assert response.status_code == 200
+
+    def test_time_endpoint(self):
+        c = Client()
+        response = c.get('/info/current/time')
+        assert response.status_code == 200
+
+    def test_number_endpoint(self):
+        c = Client()
+        response = c.get('/info/phone_number/6786786789')
+        assert response.status_code == 200
+
+    def test_countrycode_endpoint(self):
+        c = Client()
+        response = c.get('/info/phone_number/9884287789/IN')
+        assert response.status_code == 200
